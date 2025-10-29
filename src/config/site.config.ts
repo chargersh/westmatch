51e0a860453974ae609e4726bc5d098367512548
chargerhustle <chargerhustle@gmail.com>
2025-10-30 01:43:23 +0500

    added metadata

---
+++ src/config/site.config.ts
@@ -0,0 +1,17 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "NextSpark",
+  title: "Next.js + React Router",
+  description:
+    "A minimal template showcasing React Router integration within Next.js for client-side routing",
+  origin: "https://nextspark-sigma.vercel.app",
+  keywords: [
+    "React Router",
+    "Next.js",
+    "Client-side routing",
+    "React",
+    "TypeScript",
+    "Template",
+  ],
+};
---
+++ src/types/index.d.ts
@@ -0,0 +1,7 @@
+export type SiteConfig = {
+  name: string;
+  title: string;
+  description: string;
+  origin: string;
+  keywords: string[];
+};
