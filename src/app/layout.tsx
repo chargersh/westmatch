0dfb33ffff377bb2ae290c1fb6e370aed089e464
chargerhustle <chargerhustle@gmail.com>
2025-10-30 01:54:28 +0500

    add dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { siteConfig } from "@/config/site.config";
+import Providers from "@/providers";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,11 +28,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { ThemeToggler } from "@/components/theme-toggler";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+        <div className="absolute top-4 right-4">
+          <ThemeToggler />
+        </div>
         <Image
           alt="Next.js logo"
           className="dark:invert"
